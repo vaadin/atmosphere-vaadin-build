f4f226236f7418838622b6d3180f12015fbf16c9
Artur Signell <artur@vaadin.com>
2013-11-04 21:33:04 +0200

    Avoid replacing version for external org.atmosphere references
    Change-Id: I99b9cf1be86f8a92f049ed34510b420529d6ff5a

--- src/main/java/com/vaadin/external/atmosphere/build/xmlfilefilter/AtmosphereDependencyUpdater.java
+++ src/main/java/com/vaadin/external/atmosphere/build/xmlfilefilter/AtmosphereDependencyUpdater.java
@@ -25,6 +25,18 @@ public class AtmosphereDependencyUpdater extends PomXmlFilter {
 				"//groupId[starts-with(text(),'org.atmosphere')]");
 		for (int i = 0; i < nodes.getLength(); i++) {
 			Element groupId = (Element) nodes.item(i);
+			Element version = (Element)findNode(groupId,"../version");
+			if (version != null) {
+				String versionInPom = version.getTextContent();
+				String versionBeingCompiled = System.getProperty("newVersion");
+				if (!versionInPom.equals(versionBeingCompiled) && !versionInPom.startsWith("${") && !versionInPom.contains("vaadin")) {
+					// dependency to some org.atmosphere module with a different version than we are compiling. This is likely an external dependency
+					System.out.println("Not replacing '"+groupId.getTextContent()+"' with version '"+versionInPom+"'");
+					continue;
+				} else {
+				}
+			}
+				
 			groupId.setTextContent(groupId.getTextContent().replace(
 					"org.atmosphere", GROUP_ID_PREFIX));
 		}
