9f9b3376f27e2ca1349b659dc01adf698fbbc1c0
Tapio Aali <tapio@vaadin.com>
2015-02-11 09:25:22 +0200

    Fix character encoding if default is other than UTF-8

--- src/main/java/com/vaadin/external/atmosphere/build/filefilter/AbstractFileFilter.java
+++ src/main/java/com/vaadin/external/atmosphere/build/filefilter/AbstractFileFilter.java
@@ -1,33 +1,44 @@
 package com.vaadin.external.atmosphere.build.filefilter;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 public abstract class AbstractFileFilter implements FileFilter {
 
-	private File root;
+    private File root;
 
-	public AbstractFileFilter(File root) {
-		this.root = root;
-	}
+    public AbstractFileFilter(File root) {
+        this.root = root;
+    }
 
-	protected void writeFile(File f, String string) throws IOException {
-		FileWriter writer = new FileWriter(f);
-		writer.write(string);
-		writer.close();
-	}
+    protected void writeFile(File f, String string) throws IOException {
+        BufferedWriter writer = new BufferedWriter(
+            new OutputStreamWriter(new FileOutputStream(f),
+                StandardCharsets.UTF_8));
+        writer.write(string);
+        writer.close();
+    }
 
-	protected String readFile(File f) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(f));
-		int bytesRead;
-		char[] buf = new char[60000];
-		StringBuilder contents = new StringBuilder();
-		while ((bytesRead = reader.read(buf)) > 0) {
-			contents.append(buf, 0, bytesRead);
-		}
-		reader.close();
-		return contents.toString();
-	}
+    protected String readFile(File f) throws IOException {
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(new FileInputStream(f),
+                StandardCharsets.UTF_8));
+        int bytesRead;
+        char[] buf = new char[60000];
+        StringBuilder contents = new StringBuilder();
+        while ((bytesRead = reader.read(buf)) > 0) {
+            contents.append(buf, 0, bytesRead);
+        }
+        reader.close();
+        return contents.toString();
+    }
 }
