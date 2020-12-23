e5ec08b567a89a5d3df75a6e59612fb12ccd6c24
Tatu Lund <tatu@vaadin.com>
2020-12-23 12:17:16 +0200

    Add 1.8.0-beta4
    https://github.com/vaadin/atmosphere/commit/378e5f9a975b7a6cfb00f6f6aefbe39a9f94eb10

--- src/main/java/com/vaadin/external/atmosphere/build/xmlfilefilter/SLF4JRemovalValidator.java
+++ src/main/java/com/vaadin/external/atmosphere/build/xmlfilefilter/SLF4JRemovalValidator.java
@@ -35,6 +35,9 @@ public class SLF4JRemovalValidator extends PomXmlFilter {
                 } else if (line
                     .contains("<slf4j-version>1.7.7</slf4j-version>")) {
                     // this is ok
+                } else if (line
+                    .contains("<slf4j-version>1.8.0-beta4</slf4j-version>")) {
+                    // this is ok
                 } else {
                     throw new IllegalStateException("File " + f.getPath()
                         + " contains slf4j: " + line);
